4041a1089af8ec0abd30b34d8892dffd0839258d
Eileen <eileenburdekin@proton.me>
2023-12-23 17:52:42 -0800

    added initial React Navigation

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "#D9695F",
+  },
+});
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>can anyone see me??!!!!!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={styles.title}>Make Deaths Count</Text>
+      <Button
+        color="#D9695F"
+        title="Sign In"
+        onPress={() => navigation.navigate("Sign In")}
+      />
+      <Button
+        color="#D9695F"
+        title="Create Account"
+        onPress={() => navigation.navigate("Create Account")}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+function SignInScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Sign In</Text>
+      <Button
+        title="Back to Home"
+        onPress={() => navigation.navigate("Home")}
+      />
+    </View>
+  );
+}
+
+function CreateAccountScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Create Account</Text>
+      <Button
+        title="Back to Home"
+        onPress={() => navigation.navigate("Home")}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Sign In" component={SignInScreen} />
+        <Stack.Screen name="Create Account" component={CreateAccountScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
