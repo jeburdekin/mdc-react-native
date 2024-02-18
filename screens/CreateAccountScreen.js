4fe88faa8e3cd5ddfddc5a85546a15389823a781
Josephine Burdekin <Josephine@JosephiMacBook3.stdusr.yale.internal>
2024-02-17 21:47:31 -0500

    add latest changes

--- screens/CreateAccountScreen.js
+++ screens/CreateAccountScreen.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -8,17 +9,53 @@ import {
   TextInput,
   TouchableOpacity
 } from "react-native";
+import RNPickerSelect from "react-native-picker-select";
+import DatePicker from "react-native-date-picker";
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: '#fffcf7', 
+  },
   input: {
     height: 40,
     margin: 12,
-    borderWidth: 1,
+    borderWidth: 0,
     padding: 10,
+    fontSize: 14,
     backgroundColor: "white",
+  },
+  pickerContainer: { 
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    backgroundColor: "white", 
+    justifyContent: 'center',
   },
   button: {
     backgroundColor: '#D9695F', width: 175, padding: 12, borderRadius:10, marginTop: 20,
+  },
+  inputIOS: {
+    fontSize: 14,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 0,
+    color: 'black',
+    paddingRight: 30, 
+  },
+  inputAndroid: {
+    fontSize: 14,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderWidth: 0,
+    borderColor: 'grey',
+    borderRadius: 0,
+    color: 'black',
+    paddingRight: 30,
   },
   buttonText: {
     textAlign: "center",
@@ -28,22 +65,92 @@ const styles = StyleSheet.create({
 });
 
 export default function CreateAccountScreen({ navigation }) {
-  const [user, onChangeUser] = React.useState("Email Address or Phone Number");
+  const [user, onChangeUser] = React.useState("");
   const [password, onChangePassword] = React.useState("");
+  const [gender, setGender] = React.useState("");
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Sign In</Text>
+    <View style={styles.container}>
       <SafeAreaView>
         <TextInput
           style={styles.input}
           onChangeText={onChangeUser}
           value={user}
+          placeholder="Email Address or Phone Number" // Set the placeholder text here
+          placeholderTextColor="black"
         />
         <TextInput
           style={styles.input}
-          // secureTextEntry="true"
+          secureTextEntry={true}
           placeholder="Password"
+          placeholderTextColor="black"
+          onChangeText={onChangePassword}
+          value={password}
+        />
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          placeholder="Confirm password"
+          placeholderTextColor="black"
+          onChangeText={onChangePassword}
+          value={password}
+        />
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          placeholder="Interviewer code, if applicable"
+          placeholderTextColor="black"
+          onChangeText={onChangePassword}
+          value={password}
+        />
+        <Button title="Click to select age" onPress={() => setOpen(true)} />
+        <DatePicker
+          modal
+          open={open}
+          date={date}
+          mode="date"
+          onConfirm={(date) => {
+            setOpen(false)
+            setDate(date)
+         }}
+          onCancel={() => {
+            setOpen(false)
+          }}
+        />
+        <View style={styles.pickerContainer}>
+          <RNPickerSelect
+            onValueChange={(value) => setGender(value)}
+            items={[
+              { label: "Male", value: "male" },
+              { label: "Female", value: "female" },
+              { label: "Other", value: "other" },
+            ]}
+            style={{
+              inputIOS: styles.inputIOS,
+              inputAndroid: styles.inputAndroid,
+              iconContainer: {
+                top: 20,
+                right: 10,
+              },
+              placeholder: {
+                color: "black",
+                fontSize: 14,
+              }
+            }}
+            useNativeAndroidPickerStyle={false}
+            placeholder={{
+              label: "Click to select gender",
+              value: null,
+            }}
+          />
+        </View>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          placeholder="Select Location"
+          placeholderTextColor="black"
           onChangeText={onChangePassword}
           value={password}
         />
@@ -51,11 +158,11 @@ export default function CreateAccountScreen({ navigation }) {
       <TouchableOpacity onPress={() => navigation.navigate("Make Deaths Count")}
         style={styles.button}>
         <Text style={styles.buttonText}>Back to Home</Text>
-        </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("User Home")}
-        style={styles.button}>    
-      <Text style={styles.buttonText}>Create Account</Text></TouchableOpacity>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("User Home")}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Create Account</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -6,24 +6,40 @@ import {
   Text,
   Button,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  Image
 } from "react-native";
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   input: {
     height: 40,
+    width: 250,
     margin: 12,
     borderWidth: 1,
     padding: 10,
     backgroundColor: "white",
   },
   button: {
-    backgroundColor: '#D9695F', width: 175, padding: 12, borderRadius:10, marginTop: 20,
+    backgroundColor: '#D9695F',
+    width: 175,
+    padding: 12,
+    borderRadius: 10,
+    marginTop: 20,
   },
   buttonText: {
     textAlign: "center",
     color: "#fffcf7",
     fontSize: 20,
+  },
+  image: {
+    width: 100, 
+    height: 100,
+    resizeMode: 'contain', 
   }
 });
 
@@ -32,30 +48,31 @@ export default function SignInScreen({ navigation }) {
   const [password, onChangePassword] = React.useState("");
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Sign In</Text>
-      <SafeAreaView>
+    <View style={styles.container}>
+      <SafeAreaView style={{width: '100%', alignItems: "center"}}>
+        <Text>Sign In</Text>
         <TextInput
           style={styles.input}
           onChangeText={onChangeUser}
           value={user}
+          placeholder="Email Address or Phone Number"
         />
         <TextInput
           style={styles.input}
-          // secureTextEntry="true"
+          secureTextEntry={true} 
           placeholder="Password"
           onChangeText={onChangePassword}
           value={password}
         />
-      </SafeAreaView>
-      <TouchableOpacity onPress={() => navigation.navigate("Make Deaths Count")}
-        style={styles.button}>
-        <Text style={styles.buttonText}>Back to Home</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Make Deaths Count")}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Back to Home</Text>
         </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("User Home")}
-        style={styles.button}>    
-      <Text style={styles.buttonText}>Sign In</Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("User Home")}
+          style={styles.button}>    
+          <Text style={styles.buttonText}>Sign In</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     </View>
   );
 }
